6aa4389c100e6dd6b736904913ebb157f3d43917
Yanin Aleksey <aleksei.yanin.qa@gmail.com>
2024-12-20 09:36:26 +0300

    добавил книг

--- src/main/java/com/greatbit/Application.java
+++ src/main/java/com/greatbit/Application.java
@@ -12,11 +12,18 @@ public class Application {
 
     public static void main(String[] args) {
         BooksStorage.getBooks().add(
-                new Book("Учение Дона Хуана", "Карлос Кастанеда", 400)
+                new Book("Учение Дона Хуана", "Виктор Пелевин", 400)
         );
         BooksStorage.getBooks().add(
                 new Book("Богатый Папа, Бедный Папа", "Роберт Киосаки", 300)
         );
+        BooksStorage.getBooks().add(
+                new Book("Колыбель для кошки", "Воннегут Курт", 250)
+        );
+        BooksStorage.getBooks().add(
+                new Book("Мифогенная любовь каст", "Пепперштейн Павел", 576)
+        );
+
         SpringApplication.run(Application.class, args);
     }
 }
