a8610168fc3e782ea364346f4ec1933d894b502c
Yanin Aleksey <aleksei.yanin.qa@gmail.com>
2024-12-20 22:39:45 +0300

    Доработал html

--- src/main/java/com/greatbit/Application.java
+++ src/main/java/com/greatbit/Application.java
@@ -1,7 +1,5 @@
 package com.greatbit;
 
-import com.greatbit.models.Book;
-import com.greatbit.models.BooksStorage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,7 +9,6 @@ public class Application {
 
 
     public static void main(String[] args) {
-
 
         SpringApplication.run(Application.class, args);
     }
