39c86ba35fff041ab58a30604720d2a1fbc362f6
Yanin Aleksey <aleksei.yanin.qa@gmail.com>
2024-12-20 23:37:20 +0300

    Доработал html

--- src/main/java/com/greatbit/controllers/BooksController.java
+++ src/main/java/com/greatbit/controllers/BooksController.java
@@ -4,7 +4,6 @@ import com.greatbit.models.Book;
 import org.springframework.ui.Model;
 import com.greatbit.models.BooksStorage;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
